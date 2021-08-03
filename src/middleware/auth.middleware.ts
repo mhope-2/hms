1b149da0ad0eebcb565c8625d276a2fad136798b
Michael Hope Setriakor <hopemichael720@gmail.com>
2021-08-03 12:15:58 +0000

    refactor(): refactored auth

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -6,23 +6,22 @@ import UserNotExistException from '../exceptions/auth/UserNotExistException';
 import DataStoredInToken from '../interfaces/dataStoredInToken.interface';
 import RequestWithUser from '../interfaces/requestWithUser.interface';
 import UserModel from '../models/user.model';
- 
-async function authMiddleware(req: RequestWithUser, res: Response, next: NextFunction) {
-  const cookies = req.cookies;
+
+async function authMiddleware(request: RequestWithUser, response: Response, next: NextFunction) {
+  const cookies = request.cookies;
   if (cookies && cookies.Authorization) {
-    const secret = process.env.JWT_REFRESH_SECRET || '';
+    const secret = process.env.JWT_SECRET || '';
     try {
       const verificationResponse = jwt.verify(cookies.Authorization, secret) as DataStoredInToken;
       const id = verificationResponse._id;
       const user = await UserModel.findById(id);
       if (user) {
-        req.user = user;
+        request.user = user;
         next();
       } else {
-        next(new UserNotExistException());
+        next(new WrongAuthenticationTokenException());
       }
     } catch (error) {
-      console.log(error)
       next(new WrongAuthenticationTokenException());
     }
   } else {
